b574049d9199bec71fbf1943ede0979804fc3efd
Jiehmo <93374367+Jiehmo@users.noreply.github.com>
2025-05-08 14:20:22 +0800

    Update main.cpp

--- ex5/main.cpp
+++ ex5/main.cpp
@@ -1,53 +1,106 @@
-// 基礎Car類別 (SuperClass/BaseClass)
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Base Car class
 class Car {
-    protected String brand;
-    protected String driveMode;
+private:
+    string m_DriveMode;
+    int m_MaxSeating;
+    int m_price;
     
-    public Car(String brand, String driveMode) {
-        this.brand = brand;
-        this.driveMode = driveMode;
-        System.out.println("Constructing " + this.getClass().getSimpleName());
-        System.out.println(brand + " : Drive Mode = " + driveMode);
+    void m_UpdatePrice(int base = 500000) {
+        m_price = m_MaxSeating * base;
     }
     
-    public void drive() {
-        System.out.println(brand + " is driving with " + driveMode + " drive mode");
+public:
+    string m_brand;
+    string m_model;
+    int m_year;
+    
+    Car(string x, string y, int z, int s) {
+        m_brand = x;
+        m_model = y;
+        m_year = z;
+        m_MaxSeating = s;
+        m_UpdatePrice();
+        m_DriveMode = "No-wheel";
     }
-}
+    
+    int get_MaxSeating() {
+        return m_MaxSeating;
+    }
+    
+    int get_Price() {
+        return m_price;
+    }
+    
+    string get_DriveMode() {
+        return m_DriveMode;
+    }
+};
 
-// BMW車子類別，使用後輪驅動
-class BMW_Car extends Car {
-    public BMW_Car() {
-        super("BMW", "Rear-wheel");
+// BMW_Car class implementation
+class BMW_Car : public Car {
+private:
+    string m_DriveMode;
+    
+public:
+    BMW_Car(string y, int z, int s) : Car("BMW", y, z, s) {
+        cout << "Constructing BMW_Car\n";
+        m_DriveMode = "Rear-wheel";
     }
-}
+    
+    string get_DriveMode() {
+        return m_DriveMode;
+    }
+};
 
-// AUDI車子類別，使用前輪驅動
-class AUDI_Car extends Car {
-    public AUDI_Car() {
-        super("Audi", "Front-wheel");
+// AUDI_Car class implementation
+class AUDI_Car : public Car {
+private:
+    string m_DriveMode;
+    
+public:
+    AUDI_Car(string y, int z, int s) : Car("AUDI", y, z, s) {
+        cout << "Constructing AUDI_Car\n";
+        m_DriveMode = "Front-wheel";
     }
-}
+    
+    string get_DriveMode() {
+        return m_DriveMode;
+    }
+};
 
-// BENZ車子類別，使用前輪驅動
-class BENZ_Car extends Car {
-    public BENZ_Car() {
-        super("Benz", "Front-wheel");
+// BENZ_Car class implementation
+class BENZ_Car : public Car {
+private:
+    string m_DriveMode;
+    
+public:
+    BENZ_Car(string y, int z, int s) : Car("BENZ", y, z, s) {
+        cout << "Constructing BENZ_Car\n";
+        m_DriveMode = "Front-wheel";
     }
-}
+    
+    string get_DriveMode() {
+        return m_DriveMode;
+    }
+};
 
-// 主程式測試類別
-public class CarTest {
-    public static void main(String[] args) {
-        System.out.println("創建與測試汽車物件：");
-        
-        BMW_Car bmw = new BMW_Car();
-        AUDI_Car audi = new AUDI_Car();
-        BENZ_Car benz = new BENZ_Car();
-        
-        System.out.println("\n測試驅動方法：");
-        bmw.drive();
-        audi.drive();
-        benz.drive();
-    }
+// Main function
+int main() {
+    BMW_Car car_1("X5", 2023, 6);
+    cout << car_1.m_brand;
+    cout << " : Drive Mode = " << car_1.get_DriveMode() << endl;
+    
+    AUDI_Car car_2("A1", 2023, 5);
+    cout << car_2.m_brand;
+    cout << " : Drive Mode = " << car_2.get_DriveMode() << endl;
+    
+    BENZ_Car car_3("C200", 2023, 5);
+    cout << car_3.m_brand;
+    cout << " : Drive Mode = " << car_3.get_DriveMode() << endl;
+    
+    return 0;
 }
